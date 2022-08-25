02b8f623a3f020ac3ca91efaa3fd00f489fe5940
YTE4YWY2YzUzZDZhODRi <108173010+YTE4YWY2YzUzZDZhODRi@users.noreply.github.com>
2022-08-25 08:51:09 +0800

    Create cloud1.sh

---
+++ cloud1.sh
@@ -0,0 +1,4 @@
+mkdir /root/sh
+cd /root/sh
+wget https://github.com/cloudreve/Cloudreve/releases/download/3.5.3/cloudreve_3.5.3_linux_amd64.tar.gz
+wget -N https://raw.githubusercontent.com/misaka-gh/MisakaLinuxToolbox/master/MisakaToolbox.sh && chmod +x MisakaToolbox.sh && cp MisakaToolbox.sh /root/tool.sh
